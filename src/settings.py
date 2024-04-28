33cdddce4c7c02afa625aebb286c60c0435cfd22
deep <deep@Rajibuls-MacBook-Air.local>
2024-04-28 19:10:48 +0530

    fix

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,6 @@
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 azure_subscription_id = os.getenv("AZURE_SUBSCRIPTION_ID")
 azure_storage_access_key = os.getenv("AZURE_STORAGE_ACCESS_KEY")
 default_location = "eastus"
